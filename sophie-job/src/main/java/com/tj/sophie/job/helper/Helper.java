fb8388adcd24293f575c5beafa9d6fadbdf5e7aa
wangzhiheng <wangzhiheng@tigerjoys.com>
2015-06-10 15:55:00 +0800

    加入对象比较的helper

---
+++ sophie-job/src/main/java/com/tj/sophie/job/helper/Helper.java
@@ -0,0 +1,18 @@
+package com.tj.sophie.job.helper;
+
+/**
+ * Created by mbp on 6/10/15.
+ */
+public final class Helper {
+
+    public static <T extends Object> boolean equals(T x, T y) {
+        if (x == null || y == null) {
+            return false;
+        }
+        return x == y;
+    }
+
+    public static boolean isNullOrEmpty(String x) {
+        return x == null || x.trim().isEmpty();
+    }
+}
