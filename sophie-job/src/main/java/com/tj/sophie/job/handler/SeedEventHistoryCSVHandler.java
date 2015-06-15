ebc4ce9510ed31a97e5db013ac599c1f27eee011
zhaogang <zhaogang@tigerjoys.com>
2015-06-15 21:16:03 +0800

    添加MAPkey静态变量类

--- sophie-job/src/main/java/com/tj/sophie/job/handler/SeedEventHistoryCSVHandler.java
+++ sophie-job/src/main/java/com/tj/sophie/job/handler/SeedEventHistoryCSVHandler.java
@@ -6,12 +6,11 @@ import com.tj.sophie.core.AbstractHandler;
 import com.tj.sophie.core.IContext;
 import com.tj.sophie.guice.Handler;
 import com.tj.sophie.job.Actions;
+import com.tj.sophie.job.model.ContextKeys;
 import com.tj.sophie.job.model.SeedEventHistoryCSVFormatter;
 import com.tj.sophie.job.service.IGeneralCSVService;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by mbp on 11/5/15.
@@ -29,7 +28,7 @@ public class SeedEventHistoryCSVHandler extends AbstractHandler {
 
     @Override
     protected void onExecute(IContext context) {
-        List<JsonObject> eventJsonObjects = context.getVariable("events");
+        List<JsonObject> eventJsonObjects = context.getVariable(ContextKeys.EVENTS);
         if(eventJsonObjects == null || eventJsonObjects.isEmpty()){
             return;
         }
@@ -37,7 +36,7 @@ public class SeedEventHistoryCSVHandler extends AbstractHandler {
             try {
                 SeedEventHistoryCSVFormatter seedEventHistoryCSVFormatter = csvService.transfer(SeedEventHistoryCSVFormatter.class, eventJsonObjects);
                 if(seedEventHistoryCSVFormatter != null && !seedEventHistoryCSVFormatter.getExtractCSVList().isEmpty()){
-                    context.getMap("csv").put("events", seedEventHistoryCSVFormatter.getExtractCSVList());
+                    context.getMap(ContextKeys.CSVLIST).put(ContextKeys.EVENTS, seedEventHistoryCSVFormatter.getExtractCSVList());
                 }
 
             } catch (Exception e) {
