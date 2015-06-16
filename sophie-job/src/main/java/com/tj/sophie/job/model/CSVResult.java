e392afef26ebf6c2756aed900e206ca87f41fed6
zhaogang <zhaogang@tigerjoys.com>
2015-06-16 11:11:12 +0800

    修复CSVResult注解错误。

--- sophie-job/src/main/java/com/tj/sophie/job/model/CSVResult.java
+++ sophie-job/src/main/java/com/tj/sophie/job/model/CSVResult.java
@@ -1,10 +1,14 @@
 package com.tj.sophie.job.model;
 
-import java.util.*;
+import com.tj.sophie.guice.Binding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by evan.chiu on 2015/6/16.
  */
+@Binding(from = ICSVResult.class, to = CSVResult.class)
 public class CSVResult implements ICSVResult{
     private boolean initialized = false;
     private List<String> extractCSVList;
